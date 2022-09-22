f4a666ed092b9220b95f7b519af1970aa5a93e70
unknown <trungnb34@gmail.com>
2022-09-22 10:51:10 +0700

    add site post and tag

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Tag, Post
+
+admin.site.register(Tag)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug" : ("title",)}
+    list_display = ("title", "puslished_at")
+    
